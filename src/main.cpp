7de21a64ceec0f8e1d2419c909da494bb042bc8c
Joshua manela <jmmanela@mtu.edu>
2014-09-01 16:59:28 -0700

    added a bunch of comments

--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,39 @@
 
 #include "main.h"
 
+//This array contains button presses which are allowed to be asynchronous.
+//Yes, there are other ways to do this, but this is cleaner IMO.
+//*If you actually want the shift/ctr-l/etc buttons then you're going
+//to have to use Asyncronous button handeling.  I can't spell good... 
 char keyarr['z'];
 
 void display () {
 
-	/* set background color */
-	glClearColor(0,1,0,0);
+	//set background color
+	glClearColor(0,0,0,0);
 
-	/* clear window */
+	//clear the window and draw the new background
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	/* draw scene */
-	glEnd();
+	/*
+	Put any glDrawing Routines Here!
+	*/
 
-	/* flush drawing routines to the window */
+	//Flush out the drawing routines to the main window
 	glFlush();
 }
 
+//put any cleanup code here i.e. make sure to not have any memory leaks!
 void cleanup () {
 	std::cout<<"Cleaning up!"<<std::endl;
 }
 
+//idle code.  This is executed when nothing else can be.
 void idle () {
 	glutPostRedisplay();
 }
 
+//Set the key value to 1 when pressed, zero when released!
 void keydown (unsigned char key, int x, int y) {
 	keyarr[key] = 1;
 }
@@ -37,22 +45,20 @@ void keyup (unsigned char key, int x, int y) {
 
 int main ( int argc, char * argv[] ) {
 
-	/* initialize GLUT, using any commandline parameters passed to the 
-	   program */
+	// start up the GLUT engine
 	glutInit(&argc,argv);
 
-	/* setup the size, position, and display mode for new windows */
-	glutInitWindowSize(500,500);
+	// setup the size, position, and display mode for new windows
+	glutInitWindowSize(WIN_WIDTH,WIN_HEIGHT);
 	glutInitWindowPosition(0,0);
 	glutInitDisplayMode(GLUT_RGB);
 
-	/* create and set up a window */
-	glutCreateWindow("Learning OpenGL w/ Pong!");
+	// make the window
+	glutCreateWindow("Learning 2D OpenGL!");
 	glutDisplayFunc(display);
 	glutKeyboardFunc(keydown);
 	glutKeyboardUpFunc(keyup);
 	glutIdleFunc(idle);
-	/* tell GLUT to wait for events */
 	
 	glutMainLoop();
 }
--- src/main.h
+++ src/main.h
@@ -5,4 +5,7 @@
 #include<GL/glut.h>
 #include<iostream>
 
+#define WIN_WIDTH  650
+#define WIN_HEIGHT 650
+
 #endif
